0d77883b8b4bc09af48ad61cd09e63f131ba8e54
Elia <elia.niubo@gmail.com>
2025-04-24 12:50:27 +0200

    Configure Vite for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/shopping-cart-react/', // ← important for GitHub Pages!
-  plugins: [react(), svgr()],
+  plugins: [react()],
 })
